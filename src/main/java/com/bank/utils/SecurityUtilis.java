937f25bbc025cb15e94a6253c4ef52696e70cdef
Kir9g <chetvergovkirill@yandex.ru>
2024-07-28 17:17:06 +0400

    version 1.4.1

--- src/main/java/com/bank/utils/SecurityUtilis.java
+++ src/main/java/com/bank/utils/SecurityUtilis.java
@@ -20,25 +20,35 @@ public class SecurityUtilis implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(5);
-        User user1 = new User();
-        user1.setUsername("User");
-        user1.setPassword((encoder.encode("12345")));
-        user1.setROLE(RoleEnum.USER);
+        if(userRepository.findByUsername("User").isEmpty()) {
+            User user1 = new User();
+            user1.setUsername("User");
+            user1.setPassword((encoder.encode("12345")));
+            user1.setROLE(RoleEnum.USER);
 
-        userRepository.save(user1);
+            userRepository.save(user1);
+        }else {
+            System.out.println("User");
+        }
+        if(userRepository.findByUsername("admin").isEmpty()) {
+            User admin = new User();
+            admin.setUsername("admin");
+            admin.setPassword(encoder.encode("12345"));
+            admin.setROLE(RoleEnum.ADMIN);
 
-        User admin = new User();
-        admin.setUsername("admin");
-        admin.setPassword(encoder.encode("12345"));
-        admin.setROLE(RoleEnum.ADMIN);
+            userRepository.save(admin);
+        }else {
+            System.out.println("admin");
+        }
+        if(userRepository.findByUsername("chel").isEmpty()) {
+            User chel = new User();
+            chel.setUsername("chel");
+            chel.setPassword(encoder.encode("12345"));
+            chel.setROLE(RoleEnum.ADMIN);
 
-        userRepository.save(admin);
-
-        User chel = new User();
-        chel.setUsername("chel");
-        chel.setPassword(encoder.encode("12345"));
-        chel.setROLE(RoleEnum.ADMIN);
-
-        userRepository.save(chel);
+            userRepository.save(chel);
+        }else {
+            System.out.println("chel");
+        }
     }
 }
